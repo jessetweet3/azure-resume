9f5d1143f82b228d5f4e6a5c228ac993da736ca0
jessetweet3 <jesse.tweedle3@gmail.com>
2023-11-03 23:23:01 -0400

    Created azure function API

--- frontend/main.js
+++ frontend/main.js
@@ -1,16 +1,20 @@
-window.addEventListener('DOMContentLoaded', (event) => {
+window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 });
 
+const functionApi = 'http://localhost:7071/api/GetResumeCounter'
 const functionApiUrl = 'https://agetresumecounter.azurewebsites.net/api/GetResumeCounter?code=bz0TpD2qTPfMred7IhNEUsaWlR3WYb90w5xK2xO5CHIVAzFu6zcL0w==';
 
-const getVisitCount = async () => {
-    try {
-        const response = await fetch(functionApiUrl);
-        const data = await response.json();
+const getVisitCount = () => {
+    let count = 30;
+    fetch(functionApi).then(response => {
+        return response.json()
+    }).then(response =>{
         console.log("Website called function API.");
-        document.getElementById("counter").innerText = data.count;
-    } catch (error) {
-        console.log("An error occurred:", error);
-    }
-};
+        count = response.count;
+        document.getElementById("counter").innerText = count;
+    }).catch(function(error){
+        console.log(error);
+    });
+    return count;
+}
